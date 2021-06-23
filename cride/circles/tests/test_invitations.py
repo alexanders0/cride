fa2a652e48403c370fee723143efc0e8571ca030
alexanders0 <alexandersn059@gmail.com>
2021-06-22 19:20:00 -0500

    DRF APITestCase

--- cride/circles/tests/test_invitations.py
+++ cride/circles/tests/test_invitations.py
@@ -1,11 +1,17 @@
 """ Invitation tests """
 
 # Django
+from cride.circles.models.memberships import Membership
 from django.test import TestCase
 
+# Django REST Framework
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 # Model
-from cride.circles.models import Circle, Invitation
-from cride.users.models import User
+from cride.circles.models import Circle, Invitation, Membership
+from cride.users.models import User, Profile
+from rest_framework.authtoken.models import Token
 
 
 class InvitationManagerTestCase(TestCase):
@@ -60,3 +66,61 @@ class InvitationManagerTestCase(TestCase):
         )
 
         self.assertNotEqual(code, invitation.code)
+
+
+class MemberInvitationAPITestCase(APITestCase):
+    """ Member invitation API test case """
+
+    def setUp(self):
+        """ Test case setup """
+        self.user = User.objects.create(
+            first_name='Alexander',
+            last_name='Sánchez',
+            email='alexander@a.com',
+            username='alexander',
+            password='holamundo'
+        )
+        self.profile = Profile.objects.create(user=self.user)
+        self.circle = Circle.objects.create(
+            name='Facultad de Ciencias',
+            slug_name='fciencias',
+            about='Grupo oficial de la Facultad de Ciencias UTPL',
+            verified=True
+        )
+        self.membership = Membership.objects.create(
+            user=self.user,
+            profile=self.profile,
+            circle=self.circle,
+            remaining_invitations=10
+        )
+
+        # Auth
+        self.token = Token.objects.create(user=self.user).key
+        self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(self.token))
+
+        # URL
+        self.url = '/circles/{}/members/{}/invitations/'.format(
+            self.circle.slug_name,
+            self.user.username
+        )
+
+    def test_response_success(self):
+        """ Verify request succeed """
+        request = self.client.get(self.url)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+    def test_invitation_creation(self):
+        """ Verify invitation are generated if none exist previously """
+
+        # Invitations in DB must be 0
+        self.assertEqual(Invitation.objects.count(), 0)
+
+        # Call member invitations URL
+        request = self.client.get(self.url)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+        # Verify new invitations were created
+        invitations = Invitation.objects.filter(issued_by=self.user)
+        self.assertEqual(invitations.count(), self.membership.remaining_invitations)
+        for invitation in invitations:
+            self.assertIn(invitation.code, request.data['invitations'])
